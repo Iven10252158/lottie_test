837db45ef41c0cdde5d10cda8fda0ac3c94aeca5
PeiYu <yupeiq1718@gmail.com>
2022-07-11 18:45:54 +0800

    build: add compression settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import viteCompression from 'vite-plugin-compression'
 import * as path from 'path'
 import vue from '@vitejs/plugin-vue'
 
@@ -9,11 +10,28 @@ export default defineConfig({
     vue(), createSvgIconsPlugin({
       iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
       symbolId: 'icon-[dir]-[name]'
-    })
+    }), viteCompression()
   ],
   resolve: {
     alias: {
       '@': path.join(__dirname, 'src')
     }
+  },
+  build: {
+    assetsDir: './static',
+    chunkSizeWarningLimit: 500,
+    minify: 'terser',
+    cssCodeSplit: true,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log']
+      },
+      output: {
+        comments: true
+      }
+    },
+    brotliSize: false
   }
 })
